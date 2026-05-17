38fcf0eb581ee283a21fd2790b4d56340834dbb7
Julien Schueller <schueller@phimeca.com>
2026-05-17 15:07:02 +0200

    Fix windows build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-cmake ${CMAKE_ARGS} -LAH -DCMAKE_BUILD_TYPE=Release -B tmp-build .
+cmake ${CMAKE_ARGS} -G Ninja -LAH -DCMAKE_BUILD_TYPE=Release -B tmp-build .
 cmake --build tmp-build --parallel ${CPU_COUNT}
 
 $PYTHON -m pip install . -vv
