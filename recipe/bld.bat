1191243c813a25130474a9fb7893838f626e116c
Julien Schueller <schueller@phimeca.com>
2026-05-17 15:07:02 +0200

    Fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,8 @@
+cmake %CMAKE_ARGS% -G Ninja -LAH -DCMAKE_BUILD_TYPE=Release -B tmp-build .
+if errorlevel 1 exit 1
+
+cmake --build tmp-build --parallel %CPU_COUNT%
+if errorlevel 1 exit 1
+
 %PYTHON% -m pip install . -vv
 if errorlevel 1 exit 1
